861857125d152dba7aa26ade751168a96093c30a
aurimas-darguzis <aurimas@darguzis.com>
2017-05-19 19:56:17 +0100

    ArrayList methods starter file

--- Data_Structure_Interfaces/arrayList.js
+++ Data_Structure_Interfaces/arrayList.js
@@ -1,9 +1,41 @@
 class ArrayList {
+  constructor () {
+    this.length = 0
+    this.data = {}
+  }
 
-	constructor () {
-		this.length = 0
-		this.data = {}
-	}
+  push (value) {
 
+  }
 
+  pop () {
+
+  }
+
+  get (index) {
+
+  }
+
+  delete (index) {
+
+  }
+
+  _collapseTo(index) {
+
+  }
 }
+
+// unit test
+describe('ArrayList', function () {
+	const range = length => Array.apply(null, {length: length}).map(Number.call, Number)
+	const abcRange = length => range(length).map(num => String.fromCharCode(97 + num))
+	let list;
+
+	beforeEach( () => {
+		list = new ArrayList()
+	})
+
+	it('constructor', () => {
+		expect(list).toEqual(jasmine.any(ArrayList))
+	})
+})
